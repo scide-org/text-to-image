0838b7a73ec6c998f62e557d356b7e190b90bc62
Andre Sousa <andre.sousa07@gmail.com>
2021-09-06 19:39:25 +0100

    Draw lines and detect line breaks.

--- src/textToImage.ts
+++ src/textToImage.ts
@@ -26,13 +26,13 @@ const defaults = {
   customHeight: 0,
   debug: false,
   debugFilename: '',
-  fontFamily: 'Helvetica',
+  fontFamily: 'Courier',
   fontPath: '',
   fontSize: 18,
   fontWeight: 'normal',
-  lineHeight: 28,
+  lineHeight: 18,
   margin: 10,
-  maxWidth: 400,
+  maxWidth: 800,
   textAlign: 'left' as const,
   textColor: '#000',
   verticalAlign: 'top',
@@ -87,63 +87,17 @@ const createTextData = (
   textContext.fillStyle = textColor;
   textContext.font = `${fontWeight} ${fontSize}px ${fontFamily}`;
   textContext.textBaseline = 'top';
+  const lines = text.split('\n');
+  let lineCount = lines.length;
 
-  // split the text into words
-  const words = text.split(' ');
-  let wordCount = words.length;
-
-  // the start of the first line
-  let line = '';
-  const addNewLines = [];
-
-  for (let n = 0; n < wordCount; n += 1) {
-    let word: string = words[n];
-
-    if (/\n/.test(words[n])) {
-      const parts = words[n].split('\n');
-      // use the first word before the newline(s)
-      word = parts.shift() || '';
-      // mark the next word as beginning with newline
-      addNewLines.push(n + 1);
-      // return the rest of the parts to the words array at the same index
-      words.splice(n + 1, 0, parts.join('\n'));
-      wordCount += 1;
-    }
-
-    // append one word to the line and see
-    // if its width exceeds the maxWidth
-    // also trim the testLine since `line` will be empty in the beginning,
-    // causing a leading white space character otherwise
-    const testLine = `${line} ${word}`.trim();
-    const testLineWidth = textContext.measureText(testLine).width;
-
-    // if the line is marked as starting with a newline
-    // OR if the line is too long, add a newline
-    if (addNewLines.indexOf(n) > -1 || (testLineWidth > maxWidth && n > 0)) {
-      // if the line exceeded the width with one additional word
-      // just paint the line without the word
-      textContext.fillText(line, textX, textY);
-
-      // start a new line with the last word
-      // and add the following (if this word was a newline word)
-      line = word;
-
-      // move the pen down
+  for (let n = 0; n < lineCount; n += 1) {
+    let line = lines[n];
+    textContext.fillText(`${line} `, textX, textY);
+    if(n < lineCount - 1) {
       textY += lineHeight;
-    } else {
-      // if not exceeded, just continue
-      line = testLine;
     }
   }
-
-  // paint the last line
-  textContext.fillText(line, textX, textY);
-
-  // increase the size of the text layer by the line height,
-  // but in case the line height is less than the font size
-  // we increase by font size in order to prevent clipping
   const height = textY + Math.max(lineHeight, fontSize);
-
   return {
     textHeight: height,
     textData: textContext.getImageData(0, 0, maxWidth, height),
